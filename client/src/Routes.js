2ec8f5cbd84ed1625dddedaffd3af206595af398
umairx97 <umair.bajwa@hotmail.com>
2019-07-04 20:51:56 +0500

    Added Header and footer

---
+++ client/src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Layout from './hoc/Layout';
+const Routes = () => {
+  return (
+
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Layout>
+
+  )
+}
+
+export default Routes
---
+++ client/src/components/Header_Footer/Header/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <header className="bck_b_light">
+                <div className="container">
+                    <div className="left">
+                        <div className="logo">
+                            Waves
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="top">
+                            LINKS
+                        </div>
+                        <div className="bottom">
+                            LINKS
+                        </div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header
